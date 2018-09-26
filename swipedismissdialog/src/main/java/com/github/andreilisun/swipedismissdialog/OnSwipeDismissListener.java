348e2a1bc1f52a26aa122baf207d3754322bc2ef
Gibson Ainge <gainge@gmail.com>
2018-09-26 15:14:45 -0600

    Removed unused swipe dismiss files

--- swipedismissdialog/src/main/java/com/github/andreilisun/swipedismissdialog/OnSwipeDismissListener.java
+++
@@ -1,7 +0,0 @@
-package com.github.andreilisun.swipedismissdialog;
-
-import android.view.View;
-
-public interface OnSwipeDismissListener {
-    void onSwipeDismiss(View view, SwipeDismissDirection direction);
-}
--- swipedismissdialog/src/main/java/com/github/andreilisun/swipedismissdialog/SwipeDismissDirection.java
+++
@@ -1,5 +0,0 @@
-package com.github.andreilisun.swipedismissdialog;
-
-public enum SwipeDismissDirection {
-    LEFT, RIGHT, TOP, BOTTOM
-}
